6824db6aca42d4f8038565599a1128682a0357ef
Nick Ryall <nick@randb.com.au>
2011-03-15 15:54:19 +1000

    first commit

---
+++ extension.driver.php
@@ -0,0 +1,97 @@
+<?php
+
+	class extension_markitup_editor extends Extension {
+		private $params = array();
+
+		public function about() {
+			return array(
+				'name'			=> 'markItUp Editor',
+				'version'		=> '1.0',
+				'release-date'	=> '2011-03-14',
+				'author'		=> array(
+					'name'			=> 'Nick Ryall',
+					'website'		=> 'http://randb.com.au/',
+					'email'			=> 'nick@randb.com.au'
+				),
+				'description'	=> 'Sets the markItUp WYSIWYG editor on textareas'
+	 		);
+		}
+		
+	/*-------------------------------------------------------------------------
+		Installation:
+	-------------------------------------------------------------------------*/		
+		
+
+		public function uninstall() {
+			$this->_Parent->Configuration->remove('markitup');
+			$this->_Parent->saveConfig();
+		}
+
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'appendAssets'
+				),
+
+				array(
+					'page'		=> '/system/preferences/',
+					'delegate'	=> 'AddCustomPreferenceFieldsets',
+					'callback'	=> 'appendPreferences'
+				)
+			);
+		}
+
+		
+		public function getFormatter() {
+			return $this->_Parent->Configuration->get('textformatter', 'markitup');
+		}
+
+		public function appendPreferences($context) {
+			$group = new XMLElement('fieldset');
+			$group->setAttribute('class', 'settings');
+			$group->appendChild(
+				new XMLElement('legend', 'markItUp Editor')
+			);
+			
+			$options = array(
+				array('Markdown',  General::Sanitize($this->getFormatter()) == 'Markdown'),
+				array('Textile',  General::Sanitize($this->getFormatter()) == 'Textile')
+			);
+
+			$formatter = Widget::Label('Text Formatter');
+			$formatter->appendChild(Widget::select(
+				'settings[markitup][textformatter]', $options
+				
+			));
+			$group->appendChild($formatter);
+
+			$context['wrapper']->appendChild($group);
+		}
+	
+		
+	/*-------------------------------------------------------------------------
+		Utitilites:
+	-------------------------------------------------------------------------*/
+		
+		public function appendAssets($context) {
+			
+			$page = $context['parent']->Page;
+			$textformatter = extension_markitup_editor::getFormatter();
+
+			$page->addStylesheetToHead(URL . '/extensions/markitup/assets/skins/simple/style.css', 'screen', 10000, true);
+			$page->addStylesheetToHead(URL . '/extensions/markitup/assets/sets/'.$textformatter.'/style.css', 'screen', 10001, true);
+			
+			$page->addScriptToHead(URL . '/extensions/markitup/assets/jquery.markitup.js', 10002, false);
+			$page->addScriptToHead(URL . '/extensions/markitup/assets/sets/'.$textformatter.'/set.js', 10003, false);
+			
+			$page->addScriptToHead(URL . '/extensions/markitup/assets/markitup.publish.js', 10004, false);
+
+		}
+
+
+
+	}
+
+?>
